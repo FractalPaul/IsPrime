b89fa73c72a330b6593aa605d38515a2cfdc006b
Paul Saletzki <39807566+FractalPaul@users.noreply.github.com>
2022-12-09 09:52:47 -0800

    Init code
    Python code to check if a number is prime.

---
+++ prime.py
@@ -0,0 +1,37 @@
+###############################
+# is a given number prime
+###############################
+import sys
+import math
+
+# number to check if prime.
+num = 2333
+
+# check for any command line arguments
+parmLen = len(sys.argv)
+if parmLen > 1:
+    canidate = sys.argv[1]
+    try:
+        num = int(canidate)
+    except: 
+        # do nothing continue on.  bad input. not a number
+        print('Number not valid:', canidate)
+
+#######################################################
+# given a number check to see if it is prime.
+# return True if Prime otherwise False for Composite.
+#######################################################
+def isPrime(num):
+    isPrime = False
+    for factors in range(2, num):
+        if num % factors == 0:
+            # is not prime
+            isPrime = False
+            break
+        else:
+            isPrime = True
+    return isPrime
+
+isp = isPrime(num)
+primeLabel = 'Prime' if isp else 'Composite'
+print(num, 'is', primeLabel)
